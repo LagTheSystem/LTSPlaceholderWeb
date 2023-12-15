ebe771990dddafda31ba060ff4e450d2af7b7324
LagTheSystem <53086599+LagTheSystem@users.noreply.github.com>
2023-12-15 21:39:53 +0000

    Initial Setup

---
+++ script.js
@@ -0,0 +1,19 @@
+let degrees = 90;
+function spin() {
+    degrees++;
+    document.body.style.backgroundImage = "linear-gradient(" + degrees + "deg, #ff738e, #da6fe1, #997efa)";
+}
+
+let keylog = [];
+let still = true;
+document.onkeydown = function (e) {
+    let keypressed = e.key;
+    keylog.push(keypressed);
+    if (keylog.toString().includes("s,p,i,n")) {
+        if (still) {
+            still = false;
+            setInterval(spin, 25);
+            spin();
+        }
+    }
+};
